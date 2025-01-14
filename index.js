36fb3a5b0cd1a7cfeccff3803abcdafb698df2f8
phcs93 <phcs93@gmail.com>
2025-01-14 20:48:31 -0300

    first commit (icon + logo + authors + navbar + i18n)

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const params = new URLSearchParams(document.location.search);
     const page = params.get("p") || "home";    
+    document.getElementById("navbar").dataset.page = page;
     if (page === "home") window.history.pushState(null, document.title, window.location.pathname);
     const html = await (await fetch(`src/pages/${page}.html`)).text();
     document.getElementById("content").innerHTML = html;
+
+    // extremely lazy i18n
+    const lang = navigator.language.toLowerCase().indexOf("pt") > -1 ? "pt" : "en";
+    document.documentElement.setAttribute("lang", lang);
+    document.getElementById("lang").value = lang;
+    document.getElementById("lang").onchange = event => {
+        document.documentElement.setAttribute("lang", event.target.value);
+    };
 
 });

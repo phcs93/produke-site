a4536f18582ebfffe5d43ed4fb69c4d90945203e
phcs93 <phcs93@gmail.com>
2025-04-07 12:13:43 -0300

    pre-render workflow

---
+++ .github/workflows/prerender.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer");
+
+const pages = ["home", "features", "nukemnet-support", "args-builder", "changelog"];
+
+(async () => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  for (const route of pages) {
+
+    const url = `http://localhost:8080/?p=${route}`;
+    await page.goto(url, { waitUntil: "networkidle0" });
+    const html = await page.content();
+
+    const outputDir =
+      route === "home"
+        ? path.join(__dirname, "../public")
+        : path.join(__dirname, "../public", route);
+
+    fs.mkdirSync(outputDir, { recursive: true });
+    fs.writeFileSync(path.join(outputDir, "index.html"), html);
+
+    console.log(`✔ ${route}`);
+
+  }
+
+  await browser.close();
+  
+})();
